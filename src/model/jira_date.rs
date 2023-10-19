8f47c57767abffef3c29113e5de8dcab5b02d9f3
jakublaba <jakub.maciej.laba@gmail.com>
2023-10-19 13:25:54 +0200

    Implement deserialization for existing data model

--- src/model/jira_date.rs
+++ src/model/jira_date.rs
@@ -1,13 +1,14 @@
 use chrono::{DateTime, Local};
-use serde::{Serialize, Serializer};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
+#[derive(Debug)]
 pub enum JiraDate<'l> {
     Absolute(DateTime<Local>),
     Relative(&'l str),
 }
 
 impl<'l> Serialize for JiraDate<'l> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
         match *self {
             JiraDate::Absolute(date) => {
                 let json = date.to_rfc3339();
@@ -16,6 +17,17 @@ impl<'l> Serialize for JiraDate<'l> {
             JiraDate::Relative(date) => {
                 serializer.serialize_str(date)
             }
+        }
+    }
+}
+
+impl<'l, 'de: 'l> Deserialize<'de> for JiraDate<'l> {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let date_str: &str = Deserialize::deserialize(deserializer)?;
+        if let Ok(date) = DateTime::parse_from_rfc3339(date_str) {
+            Ok(Self::Absolute(date.with_timezone(&Local)))
+        } else {
+            Ok(Self::Relative(date_str.clone()))
         }
     }
 }
