8cb12d411601c875e011d40e247d35a211272095
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2024-03-16 15:59:22 +0100

    Remove JiraDate and adjust model to DateTime<Utc>

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, FixedOffset, NaiveDateTime, ParseResult, TimeZone};
+use chrono::{DateTime, NaiveDateTime, ParseResult, Utc};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -9,21 +9,19 @@ pub(crate) struct Cli {
     pub(crate) name: String,
     /// Start date of the project
     #[arg(short, long, value_name = "DATE", value_parser = parse_date)]
-    pub(crate) start: DateTime<FixedOffset>,
+    pub(crate) start: DateTime<Utc>,
     /// End date of the project
     #[arg(short, long, value_name = "DATE", value_parser = parse_date)]
-    pub(crate) end: DateTime<FixedOffset>,
+    pub(crate) end: DateTime<Utc>,
     /// Amount of issues to generate
     #[arg(short, long, value_name = "AMOUNT")]
     pub(crate) issue_amount: i32,
 }
 
-fn parse_date(arg: &str) -> ParseResult<DateTime<FixedOffset>> {
+fn parse_date(arg: &str) -> ParseResult<DateTime<Utc>> {
     let s = &format!("{arg} 00:00:00");
     let naive = NaiveDateTime::parse_from_str(s, "%d-%m-%Y %H:%M:%S")?;
-    let Some(offset) = FixedOffset::east_opt(0) else {
-        panic!("Cannot parse date from cli")
-    };
+    let utc = DateTime::<Utc>::from_naive_utc_and_offset(naive, Utc);
 
-    Ok(offset.from_utc_datetime(&naive))
+    Ok(utc)
 }
--- src/model/history_entry.rs
+++ src/model/history_entry.rs
@@ -1,19 +1,21 @@
+use chrono::{DateTime, Utc};
 use serde::Serialize;
 
 use crate::model::item::Item;
-use crate::model::jira_date::JiraDate;
+use crate::model::serialize_date;
 
 #[derive(Debug, Serialize)]
 pub struct HistoryEntry<'l> {
     author: &'l str,
-    created: JiraDate<'l>,
+    #[serde(serialize_with = "serialize_date")]
+    created: DateTime<Utc>,
     items: Vec<Item<'l>>,
 }
 
 impl<'l> HistoryEntry<'l> {
     pub fn new(
         author: &'l str,
-        created: JiraDate<'l>,
+        created: DateTime<Utc>,
         items: Vec<Item<'l>>,
     ) -> Self {
         Self { author, created, items }
