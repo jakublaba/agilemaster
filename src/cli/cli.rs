909d879bbbdc7aa0a080a7cf6282e8c05870c2c4
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2024-03-16 15:30:16 +0100

    Implement cli (#11)
    * Remove sample export from main
    
    * Implement argument parsing
    
    * Improve date parsing in cli

---
+++ src/cli/cli.rs
@@ -0,0 +1,29 @@
+use chrono::{DateTime, FixedOffset, NaiveDateTime, ParseResult, TimeZone};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, author, about, long_about = None)]
+pub(crate) struct Cli {
+    /// Name of the generated project
+    #[arg(short, long, value_name = "NAME")]
+    pub(crate) name: String,
+    /// Start date of the project
+    #[arg(short, long, value_name = "START DATE", value_parser = parse_date)]
+    pub(crate) start: DateTime<FixedOffset>,
+    /// End date of the project
+    #[arg(short, long, value_name = "END DATE", value_parser = parse_date)]
+    pub(crate) end: DateTime<FixedOffset>,
+    /// Amount of issues to generate
+    #[arg(short, long, value_name = "ISSUE AMOUNT")]
+    pub(crate) issue_amount: i32,
+}
+
+fn parse_date(arg: &str) -> ParseResult<DateTime<FixedOffset>> {
+    let s = &format!("{arg} 00:00:00");
+    let naive = NaiveDateTime::parse_from_str(s, "%d-%m-%Y %H:%M:%S")?;
+    let Some(offset) = FixedOffset::east_opt(0) else {
+        panic!("Cannot parse date from cli")
+    };
+
+    Ok(offset.from_utc_datetime(&naive))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,17 @@
 use std::error::Error;
-use std::fs::File;
 use std::io::Write;
 
-use chrono::{Duration, Local};
+use clap::Parser;
 
-use crate::model::export::Export;
-use crate::model::history_entry::HistoryEntry;
-use crate::model::issue::Issue;
-use crate::model::item::Item;
-use crate::model::jira_date::JiraDate;
-use crate::model::project::Project;
-use crate::model::user::User;
+use crate::cli::cli::Cli;
 
 mod model;
+mod cli;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let export = Export::new(
-        vec![
-            User::new(
-                "jakublaba",
-                vec![],
-                true,
-                "jakub.maciej.laba@gmail.com",
-                "Jakub Łaba",
-            ),
-        ],
-        vec![
-            Project::new(
-                "kanban-test",
-                "KT",
-                vec![
-                    Issue::new(
-                        "DONE",
-                        "606affea126db9006ff5c5de",
-                        "Story",
-                        JiraDate::Absolute(Local::now() - Duration::days(30)),
-                        JiraDate::Absolute(Local::now()),
-                        "Task 1",
-                        vec![
-                            HistoryEntry::new(
-                                "606affea126db9006ff5c5de",
-                                JiraDate::Absolute(Local::now() - Duration::days(55)),
-                                vec![
-                                    Item::new(
-                                        "status",
-                                        "TO DO",
-                                        "IN PROGRESS",
-                                    )
-                                ],
-                            ),
-                            HistoryEntry::new(
-                                "606affea126db9006ff5c5de",
-                                JiraDate::Absolute(Local::now() - Duration::days(23)),
-                                vec![
-                                    Item::new(
-                                        "status",
-                                        "IN PROGRESS",
-                                        "DONE",
-                                    )
-                                ],
-                            ),
-                        ],
-                    ),
-                    Issue::new(
-                        "DONE",
-                        "606affea126db9006ff5c5de",
-                        "Story",
-                        JiraDate::Absolute(Local::now() - Duration::days(30)),
-                        JiraDate::Absolute(Local::now()),
-                        "Task 2",
-                        vec![
-                            HistoryEntry::new(
-                                "606affea126db9006ff5c5de",
-                                JiraDate::Absolute(Local::now() - Duration::days(28)),
-                                vec![
-                                    Item::new(
-                                        "status",
-                                        "TO DO",
-                                        "IN PROGRESS",
-                                    )
-                                ],
-                            ),
-                            HistoryEntry::new(
-                                "606affea126db9006ff5c5de",
-                                JiraDate::Absolute(Local::now() - Duration::days(2)),
-                                vec![
-                                    Item::new(
-                                        "status",
-                                        "IN PROGRESS",
-                                        "DONE",
-                                    )
-                                ],
-                            ),
-                        ],
-                    ),
-                ],
-            )
-        ],
-    );
-
-    let json = serde_json::to_string_pretty(&export)?;
-    let mut file = File::create("project.json")?;
-    file.write_all(json.as_bytes())?;
+    let args = Cli::parse();
+    
+    println!("{:?}", args);
 
     Ok(())
 }
